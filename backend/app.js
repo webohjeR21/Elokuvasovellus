e44248a069b87fa25e459a0f53910ce0c6235fee
Sirma2k2 <miro.pennanen@gmail.com>
2024-04-05 16:15:53 +0300

    Käyttäjän luonnin UI tehty ja frontendin ja back- endin yhdistäminen aoitettu Käyttäjän luomisessa

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,41 @@ const { fetchData } = require('./postgre');
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+require('dotenv').config();
 
 const cors = require('cors');
 app.use(cors());
+app.use(express.json())
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Allow self-signed certificates (only for development/testing)
+  }
+});
+
+client.connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch(err => {
+    console.error('Error connecting to the database:', err.message);
+  });
 
 const PORT = 3001;
+
+app.post('/register', (req, res) => {
+ 
+ const username = req.body.username
+ const password = req.body.password
+ const email = req.body.email
+ 
+  Database.query("INSERT INTO asiakkaat(uname, passwd, email) VALUES (?,?,?)", [username, password, email], (err, result) => {
+    console.log(err);
+  })
+})
 
 app.listen(PORT, async function () {
   console.log('kuuntelee porttia ' + PORT);
--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios'
+import './Create.css'
 
 export default function Create() {
+
+  const [usernameReg, setUsernameReg] = useState('')
+  const [passwordReg, setPasswordReg] = useState('')
+  const [emailReg, setEmailReg] = useState('')
+
+  const register = () => {
+    Axios.post('http://localhost:3001/register', {
+      username: usernameReg,
+      password: passwordReg,
+      email: emailReg
+    }).then((response) => {
+      console.log(response)
+    })
+  }
+
+
   return (
-    <div>
-      <div>
-        <form action=''>
-          <div>
-            <label htmlFor='email'>Email</label>
-            <input type='email' placeholder='Enter Email' />
-          </div>
-          <div>
-            <label htmlFor='password'>Password</label>
-            <input type='password' placeholder='Enter Password' />
-          </div>
-          <button className='button'>Log in </button>
-          <button className='buttonCreate'> Luo käyttäjä </button>
+    <div className='App'>
+      <div className='registration'>
+       <h1>Käyttäjän luonti</h1>
+       <label>Käyttäjänimi</label>
+       <input type="text" onChange={(e) =>
+        {setUsernameReg(e.target.value)}} />
 
-        </form>
+       <label>Sähköposti</label>
+       <input type="text" onChange={(e) =>
+        {setEmailReg(e.target.value)}} />
+
+       <label>Salasana</label>
+       <input type="password" onChange={(e) =>
+        {setPasswordReg(e.target.value)}} />
+       <button onClick={register}>Luo käyttäjä</button>
       </div>
-
-
-
     </div>
   )
 }
